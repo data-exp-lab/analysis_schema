802e79d0b35cb33d064b509a099c0f9074753092
Matthew Turk <matthewturk@gmail.com>
2019-05-10 09:26:30 -0500

    imports and cli

--- analysis_schema/__init__.py
+++ analysis_schema/__init__.py
@@ -5,3 +5,32 @@
 __author__ = """Matthew Turk"""
 __email__ = 'matthewturk@gmail.com'
 __version__ = '0.1.0'
+
+from .data_objects import (
+    Sphere,
+    Region
+    AllData,
+    DataSource,
+)
+
+from .operations import (
+    Average,
+    Sum,
+    Minimum,
+    Maximum
+    Integrate,
+    operations,
+)
+
+from .products import (
+    Projection,
+)
+
+from .quantities import (
+    UnitfulValue,
+    UnitfulArray,
+    unyt_array_model
+    Coordinate,
+    Vector,
+    Path,
+)
--- analysis_schema/cli.py
+++ analysis_schema/cli.py
@@ -3,16 +3,29 @@
 """Console script for analysis_schema."""
 import sys
 import click
+import http.server
 
+@click.group()
+def main():
+    pass
 
-@click.command()
-def main(args=None):
-    """Console script for analysis_schema."""
-    click.echo("Replace this message by putting your code into "
-               "analysis_schema.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+@main.command()
+@click.argument("name")
+@click.option("--output", default=None, help='output filename')
+def generate(name, output):
+    click.echo("Hi there!  Generating {}.".format(name))
 
+    if output is None:
+        click.echo("No output")
+    else:
+        click.echo("Outputting to {}".format(output))
+
+@main.command()
+@click.argument("editor")
+@click.option("--port", default=8000, help='Port to serve on')
+def editor(port):
+    # TODO: Make this work with the generated files
+    http.server.SimpleHTTPServer()
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
