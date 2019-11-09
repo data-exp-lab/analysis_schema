f00f6d6f96d2ad4a744ee9403a9825b7d9e9d2c6
Matthew Turk <matthewturk@gmail.com>
2019-11-09 16:55:00 -0600

    adding some field parameter information

--- analysis_schema/fields.py
+++ analysis_schema/fields.py
@@ -1,25 +1,42 @@
 import typing
 import enum
 from pydantic import BaseModel, Schema, create_model
+from . import quantities
+
+_parameter_value_types = typing.Union[
+    quantities.UnitfulArray,
+    quantities.UnitfulValue,
+    quantities.UnitfulArray,
+    quantities.UnitfulCoordinate,
+    quantities.Vector,
+    quantities.Path,
+]
+
 
 class SamplingTypeEnum(str, enum.Enum):
     cell = "cell"
     discrete = "discrete"
 
+
 class FieldParameter(BaseModel):
-    pass
+    name: str = None
+    value: _parameter_value_types = None
+
 
 class SpatialValidator(BaseModel):
     ghost_zones: int = 0
 
+
 class ParameterValidator(BaseModel):
     parameters: typing.List[str] = []
+
 
 class PropertyValidator(BaseModel):
     property_name: str
 
-FieldValidator = typing.Union[SpatialValidator, ParameterValidator,
-                              PropertyValidator]
+
+FieldValidator = typing.Union[SpatialValidator, ParameterValidator, PropertyValidator]
+
 
 class FieldDefinition(BaseModel):
     validators: typing.List[FieldValidator] = []
@@ -31,4 +48,4 @@ class FieldDefinition(BaseModel):
     display_field: bool
     vector_field: bool
     prescription: str
-    nodal_flag: typing.Tuple[int, int, int] = (0,0,0)
+    nodal_flag: typing.Tuple[int, int, int] = (0, 0, 0)
--- analysis_schema/quantities.py
+++ analysis_schema/quantities.py
@@ -1,30 +1,33 @@
 import typing
 from pydantic import BaseModel, Schema
 
+
 class UnitfulValue(BaseModel):
     value: float
     # This is a simplification -- we are not allowing for the case of full
     # unyt unit registries, etc
     unit: str = "unitary"
 
+
 class UnitfulArray(BaseModel):
     values: typing.List[float]
     unit: str = "unitary"
+    dimensionality: int = -1  # -1 for none specified
+    shape: typing.Tuple[int] = ()
+
 
 class UnitfulCoordinate(BaseModel):
-    values: typing.List[float] = Schema(
-        [0.5, 0.5, 0.5],
-        minItems = 3,
-        maxItems = 3
-    )
+    values: typing.List[float] = Schema([0.5, 0.5, 0.5], minItems=3, maxItems=3)
     unit: str = "unitary"
 
-unyt_array_model = typing.Union[typing.List[UnitfulValue],
-                                UnitfulArray]
+
+unyt_array_model = typing.Union[typing.List[UnitfulValue], UnitfulArray]
+
 
 class Vector(BaseModel):
     start_point: UnitfulCoordinate
     end_point: UnitfulCoordinate
+
 
 class Path(BaseModel):
     points: typing.List[UnitfulCoordinate]
