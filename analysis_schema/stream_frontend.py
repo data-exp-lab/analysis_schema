323c36205639f4b208a8be9599efabb7ba9c3017
Sam Walkow <samwalkow@gmail.com>
2019-11-12 13:47:39 -0600

    Finished the stream frontend type annotations with many questions.

--- analysis_schema/stream_frontend.py
+++ analysis_schema/stream_frontend.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Schema, create_model
 from typing import Dict, List, Optional, Sequence, Set, Tuple, OrderedDict, Any
 import enum
+import time
 
 from .quantities import UnitfulCoordinate, Vector, Path, UnitfulValue, UnitfulArray
 from .data_objects import DataObject
@@ -56,54 +57,117 @@ class StreamHierarchy(BaseModel):
 class StreamDataset(BaseModel):
     # astro specific stuff:
     # there's a lot I'm not sure about:
-    cosmological_simulation:
+    cosmological_simulation: int = 1
     stream_handler : StreamHandler
-    unique_identifer :
-    dimensionality :
-    omega_lambda :
-    omega_matter :
-    fields_units :
-    particle_units :
-    current_redshifts:
-    particle_types_raw:
-    hubble_constant :
-    basename :
-    domain_left_edge:
-    domain_right_edge :
-    periodicity :
+    unique_identifer : str ='CurrentTimeIdentifier'
+    dimensionality : int
+    # really not sure about the astro specific ones
+    omega_lambda : int
+    omega_matter : str
+    fields_units : str
+    particle_units : str
+    current_redshifts: float
+    particle_types_raw : Set[List]
+    hubble_constant : float = 0.0
+    basename : str
+    domain_left_edge: float
+    domain_right_edge : float
+    periodicity : str
     geometry : str = 'cartesian'
-    domain_dimensions :
-    current_time :
+    domain_dimensions : int
+    current_time : time.time()
     gamma : float = 5./3.
-    refine_by :
+    # not sure about refine_by
+    refine_by : Any
     index_class : StreamHierarchy
+    # do we have a type for function calls?
     field_info_class : StreamFieldInfo # not defined yet, should be defined in fields file
     dataset_type : str = 'stream'
 
 class StreamDictFieldHandler(BaseModel):
+    additional_fields : Tuple
 
 class StreamParticleIndex(BaseModel):
+    # dataset calls streamhandler, how to type annotation that?
+    stream_handler : Dataset
+    # I can't tell what the io type would be
+    io : Any
 
 class StreamParticleFile(BaseModel):
+    pass
 
 class StreamParticleDataset(BaseModel):
+    index_class : StreamParticleIndex
+    file_class : StreamParticleFile
+    field_info_class : StreamFieldInfo # not defined yet
+    dataset_type : str = 'stream_particles'
+    file_count : int = 1
+    filename_template : str = 'stream_file'
+    n_ref : int = 64
+    over_refine_factor : int = 1
 
-class StreamHexahedraMesh(BaseModel):
+class StreamHexahedralMesh(BaseModel):
+    connectivity_length : int = 8
+    index_offset : int = 0
 
-class StreamHexahedraHiearchy(BaseModel):
+class StreamHexahedralHiearchy(BaseModel):
+    # dataset calls streamhandler, how to type annotation that?
+    stream_handler : Dataset
+    io : Any
+    field_list : List[Set]
+    meshes : List[StreamHexahedralMesh]
 
 class StreamHexahedralDataset(BaseModel):
+    index_class : StreamHexahedralHiearchy
+    field_info_class : StreamFeildInfo # not yet defined
+    dataset_type : str = 'stream_hexahedral'
 
 class StreamOctreeSubset(BaseModel):
+    field_parameters : Dict = {}
+    # what is a base selector
+    base_selector : Any
+    # YTFieldData() is specific for field_data
+    field_data : Any
+    oct_handler : Any
+    last_mask : Any
+    last_selector_id : Any
+    current_particle_type : str = 'io'
+    num_zones : int = 1
+    ds : Dataset
+    current_fluid_type : str
+    base_region : Any
+    domain_id : int = 1
+    domain_offset : int = 1
 
 class StreamOctreeHandler(BaseModel):
+    # maybe str?
+    dataset_type : Any = None
+    # from stream handler
+    stream_handler : Dataset
+    io : Any
+    # could probably be more specific here
+    oct_handler : Dict
+    field_list : List[Set]
 
 class StreamOctreeDataset(BaseModel):
+    index_class : StreamOctreeHandler
+    field_info_class : StreamFieldInfo # not yet defined
+    dataset_type : str = 'stream_octree'
 
 class StreamUnstructuredMesh(BaseModel):
+    connectivity_length : int
+    index_offset : int = 0
 
 class StreamUnstructuredIndex(BaseModel):
+    stream_handler : Dataset
+    io : Any
+    mesh_union : Tuple[str, int]
+    field_list : List[Set]
+    meshes : List[StreamUnstructuredMesh]
 
 class StreamUnstructuredMeshDataset(BaseModel):
+    index_class : StreamUnstructuredIndex
+    field_info_class : StreamFieldInfo # not yet defined
+    dataset_type : str = 'stream_unstructured'
 
 
