c1ec92a4f47d945a2f6da3db20397f8d64a33609
Sam Walkow <samwalkow@gmail.com>
2021-06-14 16:54:48 -0400

    Change file names

--- analysis_schema/RunAnalysis.py
+++ analysis_schema/RunAnalysis.py
@@ -3,7 +3,7 @@ from BaseModelFunctions import show_plots
 from SchemaModel import ytModel
 
 # open the file where the user is entering values
-live_json = open("pydantic_instance.json")
+live_json = open("schema_instance.json")
 # assign to a variable
 live_schema = json.load(live_json)
 live_json.close()
--- analysis_schema/SaveSchemaFile.py
+++ analysis_schema/SaveSchemaFile.py
@@ -1,8 +1,8 @@
 import json
-from main import analysis_model
+from RunAnalysis import analysis_model
 import pydantic
 
 # This code will save the model to a json file, which will be referenced by the user
 
-with open("pydantic_schema_file.json", "w") as file:
+with open("schema_instance.json", "w") as file:
     file.write(analysis_model.schema_json(indent=2))
