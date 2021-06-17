6987a51a5d469b011dcb1b1ea7016059a01f97e7
Sam Walkow <samwalkow@gmail.com>
2021-06-17 14:50:31 -0400

    Added argparse and functions for RunAnalysis.py

--- analysis_schema/RunAnalysis.py
+++ analysis_schema/RunAnalysis.py
@@ -2,32 +2,34 @@ import json
 from analysis_schema.BaseModelFunctions import show_plots
 from analysis_schema.SchemaModel import ytModel
 import sys
- 
-# total arguments
-n = len(sys.argv)
-print("Total arguments passed:", n)
- 
-# Arguments passed
-print("\nName of Python script:", sys.argv[0])
- 
-print("\nArguments passed:", end = " ")
-for i in range(1, n):
-    print(sys.argv[i], end = " ")
+import argparse 
 
-# open the file where the user is entering values
-live_json = open(sys.argv[1])
-# assign to a variable
-live_schema = json.load(live_json)
-live_json.close()
-# remove schema line
-live_schema.pop('$schema')
-
-# create analysis schema model
-analysis_model = ytModel(Plot = 
-    live_schema['Plot']
-)
+def load_and_run(json_file):
+    # open the file where the user is entering values
+    live_json = open(json_file)
+    # assign to a variable
+    live_schema = json.load(live_json)
+    live_json.close()
+    # remove schema line
+    live_schema.pop('$schema')
+    # create analysis schema model
+    analysis_model = ytModel(Plot = 
+        live_schema['Plot']
+    )
+    print(show_plots(analysis_model))
 
 if __name__ == "__main__":
 
-    # print file output
-    print(show_plots(analysis_model))
+    # create a parser
+    parser = argparse.ArgumentParser(description='Handling Filenames for Analysis')
+    
+    # add the JSON file name agrument
+    parser.add_argument('JSONFile',
+                    help='Call the JSON with the Schema to run')
+    
+    args = parser.parse_args()
+
+    # run the analysis
+    load_and_run(args.JSONFile)
+
+   
