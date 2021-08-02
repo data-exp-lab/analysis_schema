c8168d2efc7855ce0360cd4d6a81e1c9ee4e15ff
Chris Havlin <chris.havlin@gmail.com>
2021-08-02 11:37:04 -0400

    another schema test

--- tests/test_schema_model.py
+++ tests/test_schema_model.py
@@ -8,3 +8,12 @@ def test_instantiation():
         cls, kwargs = _empty_model_registry[model_type]
         model = cls(**kwargs)
         assert isinstance(model, BaseModel)
+
+
+def test_schema_generation():
+    """ checks that a json can be generated from each model type """
+    for model_type in _model_types:
+        cls, kwargs = _empty_model_registry[model_type]
+        model = cls(**kwargs)
+        model_json = model.schema()
+        assert isinstance(model_json, dict)
