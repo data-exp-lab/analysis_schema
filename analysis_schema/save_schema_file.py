578baa291b757376524e7965e665c244a0195e75
Sam Walkow <samwalkow@gmail.com>
2021-09-19 12:19:58 -0500

    Adding testing diff

--- analysis_schema/save_schema_file.py
+++ analysis_schema/save_schema_file.py
@@ -4,7 +4,7 @@ from .schema_model import ytModel
 # which will be referenced by the user
 analysis_model_schema = ytModel(Data=[{"FileName": "", "DatasetName": ""}], Plot=[{}])
 
-with open("analysis_schema/yt_analysis_schema.json", "w") as file:
+with open("yt_analysis_schema.json", "w") as file:
     file.write(analysis_model_schema.schema_json(indent=2))
 
 print("Schema is has been saved!")
