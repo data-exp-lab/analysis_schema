a244c89cd3f415663f361341f0feb9509c1488e3
Sam Walkow <samwalkow@gmail.com>
2021-10-15 15:41:26 -0400

    Moved file to scratch notebooks

--- scratch_notebooks/PandasDataframeTest.py
+++ scratch_notebooks/PandasDataframeTest.py
@@ -8,25 +8,20 @@ from pandas import _typing
 
 data_dict = {'col1': [1, 2], 'col2': [3, 4]}
 df = DataFrame(data=data_dict)
-# print(df)
-# print()
 ser = Series(data=data_dict, index=["col1", "col2"])
-# print(ser)
 
-# print(DataFrame.__annotations__)
-# # print(Series.__annotations__)
-# print()
-# print(typing.get_type_hints((DataFrame)))
-# print()
+# Trying to get annotations
+print(DataFrame.__annotations__)
+print()
+print(typing.get_type_hints((DataFrame)))
 
+# Once I have annotations, can I create a model using a loop?
 for attr, anno in DataFrame.__annotations__.items():
-    #print(attr, anno)
-    model_args = dict()
     if not attr[0].startswith('_'):
-        
-        model_args[attr] = anno
         df_class = pydantic.create_model("A DataFrame", attr=(anno))
 
+
+# Can I create a model (not automated) using `BaseModel` and dataframe as a type for an attribute
 class df_model(pydantic.BaseModel):
     dataframe: DataFrame 
 
@@ -34,6 +29,3 @@ class df_model(pydantic.BaseModel):
         arbitrary_types_allowed = True
 
 print(df_model.schema_json(indent=2))
-#print(dir(_typing))
-
-#print(help(DataFrame))
